92f202d8aff8b1ca4d11a652a62028f54c64b44a
hackerrank <me@hackerrank.com>
2021-11-11 17:57:44 +0530

    Node js code

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    //res.send('This is about page!!!')
+    res.sendFile(path.join(__dirname, 'index.html'))
+    //res.status(500)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
