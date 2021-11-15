e523806eeeac7f1aed1dbb4ffa095f26c845b192
ishalinigupta <83172449+ishalinigupta@users.noreply.github.com>
2021-11-15 12:27:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const path = require('path')
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'index.html'))
+  //res.send('Hello World!')
 })
 
 app.get('/about', (req, res) => {
+    //res.send('Hello World!')
     //res.send('This is about page!!!')
     res.sendFile(path.join(__dirname, 'index.html'))
     //res.status(500)
