603416738c781f22279440af6ac14a8710d9bceb
ishalinigupta <83172449+ishalinigupta@users.noreply.github.com>
2021-11-16 12:02:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 8080
+const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
